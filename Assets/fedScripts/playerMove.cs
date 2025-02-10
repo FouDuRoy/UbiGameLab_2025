4f414c8fd4e39c3356993d085e905c49c50df894
fedriko <166320114+fedriko@users.noreply.github.com>
2025-02-09 19:10:17 -0500

    Syteme ejection blocs

--- Assets/fedScripts/playerMove.cs
+++ Assets/fedScripts/playerMove.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Switch;
 
 public class PlayerMouvement : MonoBehaviour
 {
     [SerializeField] float speed = 1f;
     [SerializeField] float speedRotation = 1f;
+    [SerializeField] float playerCharge = 1000f;
     PlayerInput playerInput;
     InputAction moveAction;
     InputAction rotateAction;
+    InputAction throwCubes;
+
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions.FindAction("Move");
         rotateAction = playerInput.actions.FindAction("Rotate");
+        throwCubes = playerInput.actions.FindAction("ThrowCubes");
+
     }
 
     // Update is called once per frame
@@ -23,9 +29,30 @@ public class PlayerMouvement : MonoBehaviour
     {
         Vector2 direction = moveAction.ReadValue<Vector2>();
         float rotation = rotateAction.ReadValue<float>();
-        
-        transform.position += new Vector3(direction.x, 0, direction.y)*speed*Time.deltaTime; 
-        transform.Rotate(Vector3.up,rotation*speedRotation*Time.deltaTime);
-        
+
+        transform.position += new Vector3(direction.x, 0, direction.y) * speed * Time.deltaTime;
+        transform.Rotate(Vector3.up, rotation * speedRotation * Time.deltaTime);
+
+        if (throwCubes.ReadValue<float>()==1)
+        {
+            foreach (Transform child in this.transform)
+            {
+                if (child.tag != "Player")
+                {
+                    child.GetComponentInChildren<Rigidbody>().isKinematic = false;
+                    child.GetComponentInChildren<Rigidbody>().interpolation = RigidbodyInterpolation.Interpolate;
+                    child.gameObject.layer = 0;
+                    child.parent = this.transform.parent;
+                    GameObject.Destroy(child.GetChild(0).gameObject);
+                    
+                    child.GetComponentInChildren<Rigidbody>().AddExplosionForce(10000, this.transform.position, playerCharge);
+
+                }
+
+            }
+
+        }
+
     }
 }
+
