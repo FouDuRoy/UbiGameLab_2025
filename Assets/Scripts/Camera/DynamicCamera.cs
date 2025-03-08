47627a9cfb75fdd6c70f7af202e43a2fb85c1d7d
GabrielBondia <ninjagab@orange.fr>
2025-03-08 17:36:59 -0500

    Update camera

--- Assets/Scripts/Camera/DynamicCamera.cs
+++ Assets/Scripts/Camera/DynamicCamera.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.ProBuilder;
-
 public class DynamicCamera : MonoBehaviour
 {
     [SerializeField] private GameObject Player1;
