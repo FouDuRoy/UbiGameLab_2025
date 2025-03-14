06499c7abb9007e3fb0bbdd759085cf88b5153fb
GabrielBondia <ninjagab@orange.fr>
2025-03-14 19:42:48 -0400

    Ajout hapticfeedbackcontroller à player prefab

---
+++ Assets/Scripts/Player/playerControls/HapticFeedbackController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+
+public class HapticFeedbackController : MonoBehaviour
+{
+    private Gamepad playerGamepad;
+
+    void Start()
+    {
+        // Récupérer la manette liée à ce joueur (nécessite un script qui gère l'attribution des manettes)
+        var playerInput = GetComponent<PlayerInput>(); // Assurez-vous que chaque joueur a un PlayerInput
+        if (playerInput != null && playerInput.devices.Count > 0)
+        {
+            playerGamepad = playerInput.devices[0] as Gamepad;
+        }
+
+        if (playerGamepad == null)
+        {
+            Debug.LogError($"Aucune manette trouvée pour {gameObject.name}");
+        }
+    }
+
+    public void TriggerVibration(float leftMotor, float rightMotor, float duration)
+    {
+        if (playerGamepad != null)
+        {
+            playerGamepad.SetMotorSpeeds(leftMotor, rightMotor);
+            StartCoroutine(StopVibration(duration));
+        }
+    }
+
+    private IEnumerator StopVibration(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        if (playerGamepad != null)
+        {
+            playerGamepad.SetMotorSpeeds(0f, 0f);
+        }
+    }
+}
