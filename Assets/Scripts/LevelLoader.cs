393a90a826ae5c63ca2df44ff4d82bcbc0cc1642
GabrielBondia <ninjagab@orange.fr>
2025-03-21 13:06:31 -0400

    Resolve conflict Hub, ajout LevelLoader

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private SceneAsset levelToLoad;
+    [SerializeField] private int nPlayers=2;
+
+    private Collider trigger;
+    private List<GameObject> playersReady = new List<GameObject>();
+    
+
+    private void Start()
+    {
+        trigger = GetComponent<Collider>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerMouvement player=other.GetComponentInParent<PlayerMouvement>(); // Vérifie si l'objet entrant est bien un Player
+
+        if (player!=null) // Si oui, on l'ajoute à la liste des joueurs prêts
+        {
+            playersReady.Add(player.gameObject);
+            player.gameObject.SetActive(false);
+            print("ccacs");
+
+            if(playersReady.Count >= nPlayers)
+            {
+                StartCoroutine(AsyncLevelLoad(levelToLoad.name));
+            }
+        }
+    }
+
+    private IEnumerator AsyncLevelLoad(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+    }
+}
