b262c79924983959265b2cc39607c70afefc7555
fedriko <166320114+fedriko@users.noreply.github.com>
2025-03-25 21:09:31 -0400

    TwinStick

--- Assets/Scripts/Player/MouvementType.cs
+++ Assets/Scripts/Player/MouvementType.cs
@@ -2,5 +2,5 @@
 //Federico Barallobres
 public enum MouvementType
 {
-	move3d,move3dSpring,Move3dBothJoystick, Move3dBothJoystickSpring,spring,none
+	move3d,move3dSpring,Move3dBothJoystick, Move3dBothJoystickSpring,spring,none,TwinStickShooter
 }
